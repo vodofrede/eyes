09cff01e0a8bd48a2a090ba4418f0807af0f558f
vodofrede <frederik@palmoe.dk>
2023-05-24 22:24:43 +0200

    v1.3

--- src/bin/sandbox.rs
+++ src/bin/sandbox.rs
@@ -1,7 +1,12 @@
+use eyes::parse;
+
 fn main() {
     if let Some((a, b, c)) = eyes::try_parse!("1 2,3", "{} {},{}", u8, u8, u8) {
         assert!(a == 1 && b == 2 && c == 3);
     } else {
         unreachable!("This should not happen, as the pattern is matchable to the input");
     }
+
+    let input = "1,2, 3";
+    parse!(input, "{},{}, {}", i64, i64, i64);
 }
